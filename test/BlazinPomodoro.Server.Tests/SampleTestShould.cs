2afcd63e7b4806f1f5f1b3e095e6755955bbd72b
Stephen Redd <redds@reddnet.net>
2018-04-02 05:12:29 -0700

    #7 Add initial xunit test project for server

---
+++ test/BlazinPomodoro.Server.Tests/SampleTestShould.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace BlazinPomodoro.Server.Tests
+{
+    public class SampleTestShould
+    {
+        [Fact]
+        public void ReturnTrueBecauseReasons()
+        {
+            Assert.True(true, "reasons");
+        }
+    }
+}
